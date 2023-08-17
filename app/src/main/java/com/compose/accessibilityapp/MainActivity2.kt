ea9f09172b0ea59bff7416404f6893baa3e6b692
swagatRanjanDas <swagatranjan321@gmail.com>
2023-08-17 15:48:29 +0530

    Second Commit

--- app/src/main/java/com/compose/accessibilityapp/MainActivity2.kt
+++ app/src/main/java/com/compose/accessibilityapp/MainActivity2.kt
@@ -2,10 +2,13 @@ package com.compose.accessibilityapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.compose.accessibilityapp.databinding.ActivityMain2Binding
 
 class MainActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val binding = ActivityMain2Binding.inflate(layoutInflater)
+        binding.progressDx
         setContentView(R.layout.activity_main2)
     }
 }
